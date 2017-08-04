736161199ba0077ae9dc8254712b1e437883f8ff
Antonio Dolcetta <antonio.dolcetta@gmail.com>
2017-08-04 19:20:06 +1000

    bobthefish

--- config/.config/omf/init.fish
+++ config/.config/omf/init.fish
@@ -45,9 +45,28 @@ function ssh2rds -w ssh
     tab-reset
 end
 
+function load_keychain
+    if hash keychain >/dev/null ^&1
+        keychain  --agents ssh --inherit any id_rsa id_rsa_bastions
+    end
+    set keychain_file "~/.keychain/(hostname)-fish"
+    if test -n "$keychain_file"; and test -f "$keychain_file"
+        source "$keychain_file"
+    end
+end
+
+function setup_ssh_keys
+    #mwinit -k .ssh/id_rsa_bastions.pub
+    mwinit -k .ssh/id_rsa.pub
+    ssh-add -D
+    load_keychain
+end
+
 set fish_greeting ""
 
 set PATH ~/bin $PATH
 
 # install with pip install virtualfish
 eval (python3 -m virtualfish compat_aliases)
+
+load_keychain
