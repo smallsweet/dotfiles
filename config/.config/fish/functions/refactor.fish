7932997da3fc7463c5eea00de0e8cff896099d77
Antonio Dolcetta <antonio.dolcetta@gmail.com>
2017-10-12 11:16:29 +1100

    refactor fix

--- config/.config/fish/functions/refactor.fish
+++ config/.config/fish/functions/refactor.fish
@@ -1,4 +1,4 @@
-# Defined in /var/folders/1d/ycp93qn95lg261q6bg4vxkp45qsks4/T//fish.GoHpEc/refactor.fish @ line 2
+# Defined in /var/folders/1d/ycp93qn95lg261q6bg4vxkp45qsks4/T//fish.MesjRt/refactor.fish @ line 2
 function refactor --argument pattern replacement filepattern
 	if test -z "$pattern"
         echo "refactor pattern replacement [filepattern]"
@@ -8,5 +8,5 @@ function refactor --argument pattern replacement filepattern
         ag -l "$pattern"
         return
     end
-    echo ag -l "$pattern" | grep "$filepattern" | xargs sed -i ".bck" -e "s/$pattern/$replacement/"
+    echo "ag -l '$pattern' | grep '$filepattern' | xargs sed -i '' -e 's/$pattern/$replacement/'"
 end
